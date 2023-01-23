9a9e5ebfdd24c345f8b16d5a0513aa97c214455b
Alfie Freeman <runticle@gmail.com>
2023-01-23 12:55:24 +0000

    Create Page component. Setup some global styles including colours and fonts.

---
+++ src/components/Page.js
@@ -0,0 +1,82 @@
+import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+    @font-face {
+        font-family: 'textured';
+        src: url('/Cymbria-Textured.otf') format("opentype");
+        font-weight: normal;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'regular';
+        src: url('/Cymbria-Regular.otf') format("opentype");
+        font-weight: normal;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'rounded';
+        src: url('/Cymbria-Rounded.otf') format("opentype");
+        font-weight: normal;
+        font-style: normal;
+    }
+   /* setup variables */
+   html {
+      --red: #ff0000;
+      --black: #393939;
+      --grey: #3A3A3A;
+      --gray: var(--grey);
+      --lightGrey: #e1e1e1;
+      --lightGray: var(--lightGrey);
+      --offWhite: #ededed;
+      --maxWidth: 1000px;
+      --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+      box-sizing: border-box;
+      font-size: 62.5%; // set defalt font size to 10px.
+    }
+
+    *, *::before, *::after {
+        box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'rounded', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        padding: 0;
+        margin: 0;
+        font-size:1.5rem;
+        line-height: 2;
+    }
+
+    a {
+        text-decoration: none;
+        color: var(--black);
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    button {
+        font-family: 'rounded', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <GlobalStyles />
+      <InnerStyles>{children}</InnerStyles>
+    </div>
+  );
+}
+
+Page.propTypes = {
+  children: PropTypes.any,
+};
