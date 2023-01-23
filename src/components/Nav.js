5f4d641255ac1a76a3069a30468950e465023c33
Alfie Freeman <runticle@gmail.com>
2023-01-23 14:21:27 +0000

    Add 3 more pages and HOME link to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,7 @@ import NavStyles from './styles/NavStyles';
 export default function Nav() {
   return (
     <NavStyles>
+      <Link href="/">Home</Link>
       <Link href="/stuff">Stuff</Link>
       <Link href="/photos">Photos</Link>
       <Link href="/contact">Contact</Link>
