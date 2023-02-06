93b7cd50e18c788af11f60fb6c5db5f6d1ec4cbd
Alfie Freeman <runticle@gmail.com>
2023-02-06 12:40:45 +0000

    add buba page, uses Stuffs component nicely

--- src/pages/buba.js
+++ src/pages/buba.js
@@ -1,17 +1,31 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import bubaPhotoKeys from '@/utils/bubaPhotoKeys';
+import Stuffs from '@/components/Stuffs';
 
 export default function Buba() {
+  const base = `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/c_fill,w_300,h_300/v1675682672/buba-stills/`
+
+  const bubaArray = bubaPhotoKeys.map(i => base + i + '.jpg')
+
+  const stuff = bubaArray.map(i => ({
+    title: null,
+    description: null,
+    imageUrl: i,
+    action: null
+  }))
+
+  console.log('cloudname', process.env)
   return (
     <>
-        <Head>
-            <title>Alfie Freeman</title>
-            <meta name="description" content="Random stuff I like" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div>
-            Buba pics
-        </div>
+      <Head>
+        <title>Alfie Freeman</title>
+        <meta name="description" content="Buba" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Stuffs stuff={stuff} />
     </>
   )
+
 }
+
