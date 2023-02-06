4fc8669aae7e9edaeb327637cd0ea616f28e2088
Alfie Freeman <runticle@gmail.com>
2023-02-06 12:50:32 +0000

    buba introduction

--- src/pages/buba.js
+++ src/pages/buba.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import bubaPhotoKeys from '@/utils/bubaPhotoKeys';
 import Stuffs from '@/components/Stuffs';
+import TextBlock from '@/components/TextBlock';
 
 export default function Buba() {
   const base = `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/c_fill,w_300,h_300/v1675682672/buba-stills/`
@@ -14,6 +15,12 @@ export default function Buba() {
     action: null
   }))
 
+  const introduction = [
+    'Here is a selection of photos of my greatest friend, Buba.',
+    'I found her in Poland in 2019. Well, she found me.',
+    'I think she might be a mix between a mini pinscher and a jack russell, but I do not know for sure.',
+  ]
+
   return (
     <>
       <Head>
@@ -22,6 +29,8 @@ export default function Buba() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <TextBlock text={introduction} />
       <Stuffs stuff={stuff} />
     </>
   )
