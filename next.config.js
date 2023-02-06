bca5eff1e39d6ddfd1cf498e2d2d764f9e62e75c
Alfie Freeman <runticle@gmail.com>
2023-02-06 12:23:50 +0000

    Allow cloudinary urls for images

--- next.config.js
+++ next.config.js
@@ -11,6 +11,12 @@ module.exports = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
