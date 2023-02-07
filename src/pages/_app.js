76bb33e0b01b593f7f15ca23cfb4790f46243b6c
Alfie Freeman <runticle@gmail.com>
2023-02-07 14:47:54 +0000

    thebirds game doesn't use page layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 import Page from '@/components/Page'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, ...appProps }) {
+  if (['/thebirds'].includes(appProps?.router?.state?.pathname)) {
+    return (
+      <Component {...pageProps} />
+    )
+  }
+
   return (
     <Page>
       <Component {...pageProps} />
