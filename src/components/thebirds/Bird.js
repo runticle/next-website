d7fb0a347d56bad011f658f681ca71a31e18130e
Alfie Freeman <runticle@gmail.com>
2023-02-08 18:10:13 +0000

    haaa the bird is a sprite

--- src/components/thebirds/Bird.js
+++ src/components/thebirds/Bird.js
@@ -3,25 +3,21 @@ import styled from "styled-components"
 
 const BirdSprite = styled.div`
     position: absolute;
+
+    background-image: url('/images/thebirds/greenBird.png');
+    background-size: cover;
+    background-position: center;
     
-    width: 50px;
-    height: 20px;
+    width: 40px;
+    height: 40px;
 
     z-index: 10;
-    background-color: lightgreen;
 `
 
 export default function Bird({ positionMap = {}, gameStep }) {
     const nextPosition = positionMap[gameStep]
     if (!nextPosition) return null
     const { x, y } = nextPosition;
-
-
-    // if (y < 0) return null 
-
-    // console.log('bird position', position)
-
-    // console.log('Bird', position)
 
     const left = x + 'px';
     const top = y + 'px';
