9e9cfc52c6d0070f74d3fbf98cee68f10b1927e1
Alfie Freeman <runticle@gmail.com>
2023-02-08 17:23:28 +0000

    level one builder MVP

--- src/components/thebirds/levelBuilder.js
+++ src/components/thebirds/levelBuilder.js
@@ -1,32 +1,29 @@
 const level0 = [
     {
-        birdCount: 3,
-        timeStart: 0,
+        birdCount: 2,
+        gameStepStart: 0,
         maxDepth: 500
     },
-    // {
-    //     birdCount: 6,
-    //     timeStart: 50, // ms
-    //     maxDepth: 350
+    {
+        birdCount: 4,
+        gameStepStart: 50,
+        maxDepth: 350
 
-    // }, {
-    //     birdCount: 9,
-    //     timeStart: 150, // 15000ms
-    //     maxDepth: 300
-    // }
+    }, {
+        birdCount: 6,
+        gameStepStart: 100,
+        maxDepth: 200
+    }
 ];
 
-const getStartPositionsForWaveN = (count, timeStart, depth) => {
+const getStartPositionsForWaveN = (count, gameStepStart, depth) => {
     const WIDTH = 1000
-    const HEIGHT = 1000
-
-    const FLIGHT_LENGTH = 1000
 
     const SPEED = 2 // px / step
 
     const startX = Array.from({ length: count }).map((e, i) => ({
         x: (WIDTH / (count + 1)) * (i + 1),
-        y: - SPEED * timeStart,
+        y: - SPEED * gameStepStart,
         maxDepth: depth
     }))
 
@@ -38,45 +35,80 @@ const generateLevel0 = () => {
     const birdPositions = []
 
     for (const wave of level0) {
-        const start = getStartPositionsForWaveN(wave.birdCount, wave.timeStart, wave.maxDepth)
+        const start = getStartPositionsForWaveN(wave.birdCount, wave.gameStepStart, wave.maxDepth)
         birdPositions.push(start)
     }
 
-    // now we have big array of all starting positions for birds in level
-
     const startingPos = birdPositions.flat()
-
-
-    // ------------------------------------------------------------------------
-
-    //  for each wave, the max depth is set
-    // so we want to drop until that depth, and then left-right until dead
-    // we want an array of arrays now. Each bird position
-    // eventually they hover in one position
 
     const flightPaths = []
 
     for (const bird of startingPos) {
+        let step = 0
         let depth = bird.y
         const maxDepth = bird.maxDepth
 
-        // console.log(bird)
+        const birdPath = {}
 
-        const birdPath = {
-            // step: { pos }
-        }
-
-        let i = 0
-
+        // individual bird flightpath
         while (depth < maxDepth) {
-            birdPath[i] =
+            birdPath[step] =
             {
                 x: bird.x,
                 y: depth
             }
 
-            depth += 1 // speed * 10ms step
-            i += 1
+            depth += 2 // distance per step
+            step += 1
+        }
+
+        let direction = "RIGHT";
+        let move = 1
+
+        while (step < 100000) {
+
+            if (birdPath[step - 1] === undefined) {
+                console.log(birdPath.length)
+                break
+            }
+
+
+            const last_x = birdPath[step - 1]['x']
+
+            switch (direction) {
+                case 'RIGHT':
+                    if (last_x < 950) {
+                        birdPath[step] = {
+                            x: last_x + move,
+                            y: maxDepth,
+                        }
+                    } else {
+                        direction = 'LEFT'
+                        birdPath[step] = {
+                            x: last_x - move,
+                            y: maxDepth,
+                        }
+                    }
+                    break
+
+
+                case 'LEFT':
+                    if (last_x > 50) {
+                        birdPath[step] = {
+                            x: last_x - move,
+                            y: maxDepth,
+                        }
+                    } else {
+                        direction = 'RIGHT'
+                        birdPath[step] = {
+                            x: last_x + move,
+                            y: maxDepth,
+                        }
+                    }
+                    break
+            }
+
+            step += 1
         }
 
         flightPaths.push(birdPath)
