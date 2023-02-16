6f351bc1b860235bdf300ff1d930f4c291ee6b57
Alfie Freeman <runticle@gmail.com>
2023-02-16 20:33:18 +0000

    Some copy

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -9,8 +9,8 @@ const projects = [
         title: "Floating Thoughts",
         introduction: [
             "I use this for visualising my todo list.",
-            "You can add your thoughts or ideas and they float around aimlessly until you pop them.",
-            "It was very simple to code. I just used it to learn more about css transitions and animations."
+            "You can add your thoughts or ideas and they float around the screen aimlessly until you pop them.",
+            "It was very simple to code. I just used it to learn more about css transitions and animations.",
         ],
         websiteUrl: 'https://todo.alfiefreeman.co.uk',
         githubUrl: 'https://github.com/runticle/floating-thoughts',
@@ -19,9 +19,9 @@ const projects = [
     {
         title: "Garden Gremlins",
         introduction: [
-            "A simple arcade style shooting game Imade for my dads birthday",
-            "You can shoot birds and try to avoid the falling shit or you will die.",
-            "It is built in React, which isn't very good for building games, but it was a lot of fun to make."
+            "A simple arcade style shooting game I made for my dads birthday.",
+            "You fly a little rocketship and shoot birds as they come at you in waves. Try to avoid the falling shit or you will die.",
+            "It is built in React, which isn't very good for building games, but it was a lot of fun to make.",
         ],
         websiteUrl: 'https://thebirds.alfiefreeman.co.uk',
         githubUrl: 'https://github.com/runticle/garden-gremlins',
@@ -32,7 +32,9 @@ const projects = [
         title: "Makersrich",
         introduction: [
             "An educational game built in React that my group made for our final project at Makers Academy.",
-            "Sadly the repo has disappeared so it is no more."
+            "The aim was to find Rubies and Javabeans to learn more about, yup, ruby and JS. Then you try to pass the wizards' tests.",
+            "We used Firebase to enable mulitplayer, with a message board, newsfeed, and in-game interactions between online players.",
+            "Sadly the repo has disappeared so you won't have the chance to play :("
         ],
         websiteUrl: null,
         imageSrc: '/makersrich.png',
